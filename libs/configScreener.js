89c6ac3df5af8f416d0bf1067c6e9341d7ad9673
Youpit44 <upete1@gmail.com>
2018-04-28 12:43:08 +0200

    Ajout des librairies.

---
+++ libs/configScreener.js
@@ -0,0 +1,19 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+// LISTE DES COINS A MODIFIER
+////////////////////////////////////////////////////////////////////////////////////////////////////
+lstCoins.push(["BITCOIN", "coinmarketcap", 1, "", 0.10391042, ""]);
+lstCoins.push(["ETHEREUM", "coinmarketcap", 1027, "", 0.3641421, ""]);
+lstCoins.push(["LITECOIN", "coinmarketcap", 2, "", 0.202059, ""]);
+lstCoins.push(["NEO", "coinmarketcap", 1376, "", 1, ""]);
+lstCoins.push(["ARCTICCOIN", "coinmarketcap", 1434, "", 1000, ""]);
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// LISTE DES TOKENS ERC20 (Ethereum) A MODIFIER
+////////////////////////////////////////////////////////////////////////////////////////////////////
+lstTokens.push(["TRON", "coinmarketcap", 1958, "", 2003]);
+lstTokens.push(["HAVVEN", "coinmarketcap", 2586, "", 268.37]);
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// https://s2.coinmarketcap.com/static/img/coins/32x32/2474.png
+exceptTokens.push("PRO");
+exceptContract.push("0xbe428c3867f05dea2a89fc76a102b544eac7f772");
+exceptContract.push("0xd6e49800decb64c0e195f791348c1e87a5864fd7");
+var apiWebJS = true;
---
+++ libs/walletsScreener.js
@@ -0,0 +1,124 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Get Balance ArcTic Coin
+function getWallet_NEO(address) {
+	// Format : http://explorer.arcticcoin.org/ext/getbalance/address
+	// var value = 0;
+	$.ajax({
+		url: "https://neoscan.io/api/main_net/v1/get_balance/"+address,
+		type: "GET",
+		dataType: 'json',
+		async: false,
+		crossDomain: true,
+		success: function(data) {
+			// console.log(data);
+			$.each(data, function(key, value) {
+				$.each(value, function(ukey, uvalue) {
+					var newvalue = 0;
+					var tokenName = "";
+					var coinmarketcap = "";
+					
+					if(uvalue.asset == "Ontology Token") {
+						newvalue = uvalue.amount / 100000000;
+						tokenName = "ONTOLOGY";
+						coinmarketcap = "coinmarketcap";
+					}
+					else if(uvalue.asset == "CPX Token") {
+						newvalue = uvalue.amount / 100000000;
+						tokenName = "APEX";
+						coinmarketcap = "coinmarketcap";
+					}
+					else if(uvalue.asset == "Switcheo") {
+						newvalue = uvalue.amount / 100000000;
+						tokenName = "SWITCHEO";
+						coinmarketcap = "coinmarketcap";
+					}
+					else if(uvalue.asset == "Galaxy Token") {
+						newvalue = uvalue.amount / 100000000;
+						tokenName = "Galaxy";
+						coinmarketcap = "";
+					}
+					else if(uvalue.asset == "Aphelion") {
+						newvalue = uvalue.amount / 100000000;
+						tokenName = "Aphelion";
+						coinmarketcap = "";
+					}
+					else if(uvalue.asset == "Zeepin Token") {
+						newvalue = uvalue.amount / 100000000;
+						tokenName = "ZEEPIN";
+						coinmarketcap = "coinmarketcap";
+					}
+					else if(uvalue.asset == "NEO") {
+						newvalue = uvalue.amount;
+						tokenName = "NEO";
+						coinmarketcap = "coinmarketcap";
+					}
+					else if(uvalue.asset == "GAS") {
+						newvalue = uvalue.amount;
+						tokenName = "GAS";
+						coinmarketcap = "coinmarketcap";
+					}
+					if(coinmarketcap != "") {
+						if(tokenName == "NEO") {
+							lstCoins.push([tokenName, coinmarketcap, 0, "", newvalue, ""]);
+							
+							if((lstAllTokensCMC.filter(p => p.symbol == tokenName)).length > 0) {
+								var indexToken = lstAllTokensCMC.findIndex(p => p.symbol == tokenName);
+								var token = lstAllTokensCMC.filter(p => p.symbol == tokenName);
+								lstGoodCoins[token[0].rank] = [token[0].id, token[0].symbol, token[0].rank, token[0].price_btc, token[0]["percent_change_24h"], token[0].market_cap_usd, token[0]["24h_volume_usd"], parseFloat(newvalue), token[0].name, token[0].price_usd, token[0].price_eur, "Not a token"] ;
+								smallListCoins.push([(token[0].id).toUpperCase(), parseInt(token[0].rank)]);
+								// console.log(tokenName);
+							}
+							else {
+								unkTokens.push([tokenName, tokenName, 0, 0, 0, 0, 0, ""]);
+							}							
+						}
+						else {
+							lstTokens.push([tokenName, coinmarketcap, 0, "", newvalue, ""]);
+							if((lstAllTokensCMC.filter(p => (p.id).toLowerCase() == tokenName.toLowerCase())).length > 0) {
+								var indexToken = lstAllTokensCMC.findIndex(p => (p.id).toLowerCase() == tokenName.toLowerCase());
+								var token = lstAllTokensCMC.filter(p => (p.id).toLowerCase() == tokenName.toLowerCase());
+								lstGoodTokens[token[0].rank] = [token[0].id, token[0].symbol, token[0].rank, token[0].price_btc, token[0]["percent_change_24h"], token[0].market_cap_usd, token[0]["24h_volume_usd"], parseFloat(newvalue), token[0].name, token[0].price_usd, token[0].price_eur, "Not a token"] ;
+								smallListTokens.push([(token[0].id).toUpperCase(), parseInt(token[0].rank)]);
+							}
+							else {
+								unkTokens.push([tokenName, tokenName, 0, 0, 0, 0, 0, ""]);
+							}							
+						}
+					}
+				});
+			});
+		}
+	});
+	smallListCoins.sort(function(a,b){
+		return a[1] - b[1];
+	});
+	smallListTokens.sort(function(a,b){
+		return a[1] - b[1];
+	});
+	
+	return false;
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Get Balance ArcTic Coin
+function getWallet_ArcTic(address) {
+	// Format : http://explorer.arcticcoin.org/ext/getbalance/address
+	var value = 0;
+	$.ajax({
+		url: "http://spootnik.fr/wscryptos/getwallet.php?address="+address,
+		type: "GET",
+		dataType: 'html',
+		async: false,
+		//crossDomain: true,
+		success: function(data) {
+			value = data;
+		}
+	});
+	var indexToken = lstAllTokensCMC.findIndex(p => (p.id).toLowerCase() == "arcticcoin");
+	var token = lstAllTokensCMC.filter(p => (p.id).toLowerCase() == "arcticcoin");
+	lstGoodCoins[token[0].rank] = [token[0].id, token[0].symbol, token[0].rank, token[0].price_btc, token[0]["percent_change_24h"], token[0].market_cap_usd, token[0]["24h_volume_usd"], parseFloat(value), token[0].name, token[0].price_usd, token[0].price_eur, "Not a token"] ;
+	smallListCoins.push([(token[0].id).toUpperCase(), parseInt(token[0].rank)]);
+	return value;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
